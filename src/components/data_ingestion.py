062cc15d22a23ea2986195eace7bb10078e5d179
Parmeet Singh <parmeetsingh8595@gmail.com>
2024-09-18 12:35:37 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from dataclasses import dataclass
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import  DataTransformation
+from src.components.data_transformation import  DataTransformationConfig
 
 
 @dataclass
@@ -43,4 +45,6 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
