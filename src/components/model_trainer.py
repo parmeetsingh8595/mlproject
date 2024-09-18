cddb6daed1792c8f93167c4239c199e8a36a4583
Parmeet Singh <parmeetsingh8595@gmail.com>
2024-09-18 14:53:11 +0530

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,3 +14,50 @@ from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
+from src.util import save_object,evaluate_model
+
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_file_path = os.path.join("artifact","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Splitting Training and test input data")
+            x_train,y_train,x_test,y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest Regressor": RandomForestRegressor(),
+                "XGBRegressor": XGBRegressor(), 
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
+            }
+            model_report:dict = evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+        
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+            if best_model_score<=0.6:
+                raise Exception("No Best model Found")
+            logging.info("Best model found in both training and test data")
+
+
+            save_object(file_path=self.model_trainer_config.model_trainer_file_path,obj=best_model)
+            predicted=best_model.predict(x_test)
+            score  = r2_score(y_test,predicted)
+            return score
+        except Exception as e:
+            raise CustomException(e,sys)
--- src/util.py
+++ src/util.py
@@ -1,14 +1,34 @@
-import os,sys
+import os, sys
 import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
-def save_object(file_path,obj):
+from sklearn.metrics import r2_score
+
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+
+def evaluate_model(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}
+        for model_name, model in models.items():
+            model.fit(x_train, y_train)
+
+            y_pred_train = model.predict(x_train)
+            y_pred_test = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_pred_train)
+            test_model_score = r2_score(y_test, y_pred_test)
+
+            report[model_name] = test_model_score
+
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
